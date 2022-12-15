ee8da6516f033e3a73670cce1609ec37a0f5e334
rua1006 <bskang0806@naver.com>
2022-12-15 13:37:24 +0900

    "product"

--- myapp/src/main/java/com/shop/myapp/controller/ProductController.java
+++ myapp/src/main/java/com/shop/myapp/controller/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -24,4 +26,24 @@ public class ProductController {
         return "/product/productList";
     }
 
+    @GetMapping("detail")
+    public String getProduct(@RequestParam("prono") int prono, Model model) throws Exception{
+        ProductDTO dto = productService.getProduct(prono);
+        model.addAttribute("dto", dto);
+        return "/product/productDetail";
+    }
+
+    @GetMapping("category")
+    public String productCateList(@RequestParam("cateno") int cateno, Model model) throws Exception{
+        List<ProductDTO> productcateList = productService.productCateList(cateno);
+        model.addAttribute("productcateList", productcateList );
+        return "/product/productcateList";
+    }
+
+    @GetMapping("delete")
+    public String productDelte(HttpServletRequest request, Model model) throws Exception{
+        int prono = Integer.parseInt(request.getParameter("prono"));
+        productService.productDelete(prono);
+        return "redirect:list";
+    }
 }
