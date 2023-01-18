71786599e12f8531196049959b4bd9621065d48c
rua1006 <bskang0806@naver.com>
2023-01-18 16:12:02 +0900

    JPA 적용중 && ajax 적용중

--- myapp/src/main/java/com/shop/myapp/controller/CartController.java
+++ myapp/src/main/java/com/shop/myapp/controller/CartController.java
@@ -1,87 +1,87 @@
-package com.shop.myapp.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.security.Principal;
-import java.util.List;
-
-@RequiredArgsConstructor
-@Controller
-public class CartController {
-
-    private final CartService cartService;
-
-    @PostMapping(value = "/cart/")
-    public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDTO cartItemDTO,
-                                              BindingResult bindingResult, Principal principal) {
-        if(bindingResult.hasErrors()){
-            StringBuilder sb = new StringBuilder();
-            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-            for(FieldError fieldError : fieldErrors){
-                sb.append(fieldError.getDefaultMessage());
-            }
-            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
-        }
-        String email = principal.getName();
-        Long cartItemId;
-
-        try{
-            cartItemId = cartService.addCart(cartItemDTO, email);
-        }catch(Exception e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
-    }
-
-
-    @GetMapping(value="/cart/")
-    public String orderList(Principal principal, Model model){
-        List<CartDetailDTO> cartDetailDTOList = cartService.getCartList(principal.getName());
-        model.addAttribute("cartItems", cartDetailDTOList);
-        return "cart/cartList";
-    }
-    @PostMapping(value="/cartItem/{cartItemId}")
-    public @ResponseBody ResponseEntity updateCartItem(@PathVariable("cartItemId") Long cartItemId,
-                                                       int count, Principal principal){
-        if(count<=0){
-            return new ResponseEntity<String>("최소 1개 이상 담으셔야합니다.", HttpStatus.BAD_REQUEST);
-        }else if(!cartService.validateCartItem(cartItemId, principal.getName())){
-            return new ResponseEntity<String>("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
-        }
-        cartService.updateCartItemCount(cartItemId, count);
-        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
-    }
-    @DeleteMapping(value = "/cartItem/{cartItemId}")
-    public @ResponseBody ResponseEntity deleteCartItem(@PathVariable("cartItemId") Long cartItemId, Principal principal){
-        if(!cartService.validateCartItem(cartItemId, principal.getName())) {
-            return new ResponseEntity<String>("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
-        }
-        cartService.deleteCartItem(cartItemId);
-        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
-    }
-    @PostMapping(value = "/cart/orders")
-    public @ResponseBody ResponseEntity OderCartItem(
-            @RequestBody CartOrderDTO cartOrderDTO, Principal principal) {
-        List<CartOrderDTO> cartOrderDTOList = cartOrderDTO.getCartOrderDTOList();
-
-        if(cartOrderDTOList == null || cartOrderDTOList.size() == 0){
-            return new ResponseEntity<String>("주문할 상품을 선택해주세요", HttpStatus.BAD_REQUEST);
-        }
-        for(CartOrderDTO cartOrder : cartOrderDTOList){
-            if(!cartService.validateCartItem(cartOrder.getCartItemId(),principal.getName())){
-                return new ResponseEntity<String>("주문 권한이 없습니다.", HttpStatus.FORBIDDEN);
-            }
-        }
-        Long orderId = cartService.orderCartItem(cartOrderDTOList, principal.getName());
-        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
-    }
-    
-}
+//package com.shop.myapp.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.FieldError;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.validation.Valid;
+//import java.security.Principal;
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Controller
+//public class CartController {
+//
+//    private final CartService cartService;
+//
+//    @PostMapping(value = "/cart/")
+//    public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDTO cartItemDTO,
+//                                              BindingResult bindingResult, Principal principal) {
+//        if(bindingResult.hasErrors()){
+//            StringBuilder sb = new StringBuilder();
+//            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+//            for(FieldError fieldError : fieldErrors){
+//                sb.append(fieldError.getDefaultMessage());
+//            }
+//            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+//        }
+//        String email = principal.getName();
+//        Long cartItemId;
+//
+//        try{
+//            cartItemId = cartService.addCart(cartItemDTO, email);
+//        }catch(Exception e){
+//            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+//    }
+//
+//
+//    @GetMapping(value="/cart/")
+//    public String orderList(Principal principal, Model model){
+//        List<CartDetailDTO> cartDetailDTOList = cartService.getCartList(principal.getName());
+//        model.addAttribute("cartItems", cartDetailDTOList);
+//        return "cart/cartList";
+//    }
+//    @PostMapping(value="/cartItem/{cartItemId}")
+//    public @ResponseBody ResponseEntity updateCartItem(@PathVariable("cartItemId") Long cartItemId,
+//                                                       int count, Principal principal){
+//        if(count<=0){
+//            return new ResponseEntity<String>("최소 1개 이상 담으셔야합니다.", HttpStatus.BAD_REQUEST);
+//        }else if(!cartService.validateCartItem(cartItemId, principal.getName())){
+//            return new ResponseEntity<String>("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
+//        }
+//        cartService.updateCartItemCount(cartItemId, count);
+//        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+//    }
+//    @DeleteMapping(value = "/cartItem/{cartItemId}")
+//    public @ResponseBody ResponseEntity deleteCartItem(@PathVariable("cartItemId") Long cartItemId, Principal principal){
+//        if(!cartService.validateCartItem(cartItemId, principal.getName())) {
+//            return new ResponseEntity<String>("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
+//        }
+//        cartService.deleteCartItem(cartItemId);
+//        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+//    }
+//    @PostMapping(value = "/cart/orders")
+//    public @ResponseBody ResponseEntity OderCartItem(
+//            @RequestBody CartOrderDTO cartOrderDTO, Principal principal) {
+//        List<CartOrderDTO> cartOrderDTOList = cartOrderDTO.getCartOrderDTOList();
+//
+//        if(cartOrderDTOList == null || cartOrderDTOList.size() == 0){
+//            return new ResponseEntity<String>("주문할 상품을 선택해주세요", HttpStatus.BAD_REQUEST);
+//        }
+//        for(CartOrderDTO cartOrder : cartOrderDTOList){
+//            if(!cartService.validateCartItem(cartOrder.getCartItemId(),principal.getName())){
+//                return new ResponseEntity<String>("주문 권한이 없습니다.", HttpStatus.FORBIDDEN);
+//            }
+//        }
+//        Long orderId = cartService.orderCartItem(cartOrderDTOList, principal.getName());
+//        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+//    }
+//
+//}
